04b9371ad98914eff84dcca026778a1e23f01d7d
Rafael Henrique <rafaelhenrique10101@gmail.com>
2018-03-11 19:30:03 -0300

    Estrutura do header e cor de fundo

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.master')
+
+@section('body')	
+<header id="header">
+    <div class="card-head">
+    </div>
+    <div class="card-head">
+        <ul class="nav nav-tabs tabs-center" data-toggle="tabs">
+            <li class="active"><a href="">P&Aacute;GINA INICIAL</a></li>
+            <li><a href="">AO VIVO</a></li>
+            <li><a href="">JOGOS</a></li>
+            <li><a href="">CANAIS</a></li>
+        </ul>
+    </div>
+</header>
+<div id="base">	
+    <div class="offcanvas">
+    </div>
+        <div id="content">			
+            <section>                
+                @yield('content')                
+            </section>
+        </div>
+    
+</div>    
+@stop
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,44 @@
+<html lang="en">
+<head>
+		<title>NoobNever - Sua escola de games</title>
+		
+		<!-- BEGIN META -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="keywords" content="your,keywords">
+		<meta name="description" content="Short explanation about this website">
+		<!-- END META -->
+
+		<!-- BEGIN STYLESHEETS -->
+    <link href="http://fonts.googleapis.com/css?family=Roboto:300italic,400italic,300,400,500,700,900" rel="stylesheet" type="text/css">
+    <link type="text/css" rel="stylesheet" href="css/bootstrap.css">
+    <link type="text/css" rel="stylesheet" href="css/materialadmin.css">
+    <link type="text/css" rel="stylesheet" href="css/font-awesome.min.css">
+    <link type="text/css" rel="stylesheet" href="css/material-design-iconic-font.min.css">
+
+	
+		<!-- END STYLESHEETS -->
+
+
+
+    <!-- BEGIN JAVASCRIPT -->
+		
+    <script src="./js/jquery-3.2.1.min.js"></script>
+    <script src="./js/jquery-migrate-1.2.1.min.js"></script>
+    <script src="./js/bootstrap.min.js"></script>
+    <script src="./js/spin.min.js"></script>
+    <script src="./js/jquery.autosize.min.js"></script>
+    <script src="./js/moment.min.js"></script>    
+    <script src="./js/jquery.knob.min.js"></script>
+    <script src="./js/jquery.sparkline.min.js"></script>
+    <script src="./js/jquery.nanoscroller.min.js"></script>    
+    <script src="./js/rickshaw.min.js"></script>    
+    <script src="./js/Demo.js"></script>
+    <script src="./js/DemoDashboard.js"></script>
+
+	  <style type="text/css"></style>  
+  </head>
+    <body class="menubar-hoverable header-fixed">
+      @yield('body')	
+    </body>  
+  </html>
