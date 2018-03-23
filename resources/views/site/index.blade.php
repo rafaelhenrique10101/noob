97993056cc2c7ad4e32f3c1b1e6508dfdbb1de6b
Rafael Henrique <rafaelhenrique10101@gmail.com>
2018-03-22 22:17:20 -0300

    Novos thumbs de videos

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -6,47 +6,268 @@
     <div class="content-body">
         <div class="row" style="height: 270px;background-color: #000000;">
         </div>
-        <div class="row" style="height: 100%">
+        <div class="row row-border-separator">
             <h1 class="h-h1">Recomendados</h1>
             <div class="center-content-body al-center">
                 <div class="col-grid-videos col-thumb ft-left-p-rel">
                     <div class="container_thumbnail">
-                        <img src="//i.ytimg.com/vi_webp/WoGsRZVXYlg/mqdefault.webp" class="img-thumb" />
+                        <img src="https://i.ytimg.com/vi/BpI_bUr1xvE/hqdefault_live.jpg" class="img-thumb" />
+                    </div>
+                    <div class="container-info-thumb-video">
+                        <div class="container-video-title-thumb">
+                            <span class="txt-size-15px txt-weight-500 txt-white">TOP 1 DEL MUNDO SEMANAL Y TOP 1 DE ESPAÑA | CALL OF DUTY: WW2</span>
+                        </div>
+                        <div class="ft-left-p-rel w-100pc">
+                            <span class="txt-size-13px txt-weight-400 txt-grey-3">bazattak007</span>
+                        </div>
+                        <div class="ft-left-p-rel w-100pc">
+                            <span class="txt-size-13px txt-weight-500 txt-white">72 assistindo</span>
+                        </div>
                     </div>
                 </div>
                 <div class="col-grid-videos col-thumb ft-left-p-rel">
                     <div class="container_thumbnail">
-                        <img src="//i.ytimg.com/vi_webp/WoGsRZVXYlg/mqdefault.webp" class="img-thumb" />
+                        <img src="https://i.ytimg.com/vi/ZeY_PqYit_k/hqdefault_live.jpg" class="img-thumb" />
+                    </div>
+                    <div class="container-info-thumb-video">
+                        <div class="container-video-title-thumb">
+                            <span class="txt-size-15px txt-weight-500 txt-white">C9 vs. FLY - CLG vs. TSM - FOX vs. 100 - TL vs. OPT | NA LCS Spring (2018)</span>
+                        </div>
+                        <div class="ft-left-p-rel w-100pc">
+                            <span class="txt-size-13px txt-weight-400 txt-grey-3">bazattak007</span>
+                        </div>
+                        <div class="ft-left-p-rel w-100pc">
+                            <span class="txt-size-13px txt-weight-500 txt-white">72 assistindo</span>
+                        </div>
                     </div>
                 </div>
                 <div class="col-grid-videos col-thumb ft-left-p-rel">
                     <div class="container_thumbnail">
-                        <img src="//i.ytimg.com/vi_webp/WoGsRZVXYlg/mqdefault.webp" class="img-thumb" />
+                        <img src="https://i.ytimg.com/vi/zcVXIj3M9ZQ/hqdefault_live.jpg" class="img-thumb" />
+                    </div>
+                    <div class="container-info-thumb-video">
+                        <div class="container-video-title-thumb">
+                            <span class="txt-size-15px txt-weight-500 txt-white">TOP 1 DEL MUNDO SEMANAL Y TOP 1 DE ESPAÑA | CALL OF DUTY: WW2</span>
+                        </div>
+                        <div class="ft-left-p-rel w-100pc">
+                            <span class="txt-size-13px txt-weight-400 txt-grey-3">bazattak007</span>
+                        </div>
+                        <div class="ft-left-p-rel w-100pc">
+                            <span class="txt-size-13px txt-weight-500 txt-white">72 assistindo</span>
+                        </div>
                     </div>
                 </div>
                 <div class="col-grid-videos col-thumb ft-left-p-rel">
                     <div class="container_thumbnail">
-                        <img src="//i.ytimg.com/vi_webp/WoGsRZVXYlg/mqdefault.webp" class="img-thumb" />
+                        <img src="https://i.ytimg.com/vi/wDEwYYW8Pjk/hqdefault_live.jpg" class="img-thumb" />
+                    </div>
+                    <div class="container-info-thumb-video">
+                        <div class="container-video-title-thumb">
+                            <span class="txt-size-15px txt-weight-500 txt-white">TLA vs. FOXA | Semifinals Day 1 | NA Academy Spring Split (2018)</span>
+                        </div>
+                        <div class="ft-left-p-rel w-100pc">
+                            <span class="txt-size-13px txt-weight-400 txt-grey-3">bazattak007</span>
+                        </div>
+                        <div class="ft-left-p-rel w-100pc">
+                            <span class="txt-size-13px txt-weight-500 txt-white">72 assistindo</span>
+                        </div>
                     </div>
                 </div>
                 <div class="col-grid-videos col-thumb ft-left-p-rel">
                     <div class="container_thumbnail">
-                        <img src="//i.ytimg.com/vi_webp/WoGsRZVXYlg/mqdefault.webp" class="img-thumb" />
+                        <img src="https://i.ytimg.com/vi/-TgREHxSp88/hqdefault_live.jpg" class="img-thumb" />
+                    </div>
+                    <div class="container-info-thumb-video">
+                        <div class="container-video-title-thumb">
+                            <span class="txt-size-15px txt-weight-500 txt-white">*NEW* HEAVY SHOTGUN - Fortnite Battle Royale LIVE</span>
+                        </div>
+                        <div class="ft-left-p-rel w-100pc">
+                            <span class="txt-size-13px txt-weight-400 txt-grey-3">bazattak007</span>
+                        </div>
+                        <div class="ft-left-p-rel w-100pc">
+                            <span class="txt-size-13px txt-weight-500 txt-white">72 assistindo</span>
+                        </div>
                     </div>
                 </div>
                 <div class="col-grid-videos col-thumb ft-left-p-rel">
                     <div class="container_thumbnail">
-                        <img src="//i.ytimg.com/vi_webp/WoGsRZVXYlg/mqdefault.webp" class="img-thumb" />
+                        <img src="https://i.ytimg.com/vi/HgRtO4Q6JPI/hqdefault_live.jpg" class="img-thumb" />
+                    </div>
+                    <div class="container-info-thumb-video">
+                        <div class="container-video-title-thumb">
+                            <span class="txt-size-15px txt-weight-500 txt-white">RESIDENT EVIL 4 - SEM TOMAR DANO, LEVOU 1 HIT RESETA - PROFISSIONAL</span>
+                        </div>
+                        <div class="ft-left-p-rel w-100pc">
+                            <span class="txt-size-13px txt-weight-400 txt-grey-3">bazattak007</span>
+                        </div>
+                        <div class="ft-left-p-rel w-100pc">
+                            <span class="txt-size-13px txt-weight-500 txt-white">72 assistindo</span>
+                        </div>
                     </div>
                 </div>
                 <div class="col-grid-videos col-thumb ft-left-p-rel">
                     <div class="container_thumbnail">
-                        <img src="//i.ytimg.com/vi_webp/WoGsRZVXYlg/mqdefault.webp" class="img-thumb" />
+                        <img src="https://i.ytimg.com/vi_webp/YTJxPiokCvk/hqdefault.webp" class="img-thumb" />
+                    </div>
+                    <div class="container-info-thumb-video">
+                        <div class="container-video-title-thumb">
+                            <span class="txt-size-15px txt-weight-500 txt-white">Live - Fifa 2018 Teste - Primeira vez que jogo FIFA - Direto do X360</span>
+                        </div>
+                        <div class="ft-left-p-rel w-100pc">
+                            <span class="txt-size-13px txt-weight-400 txt-grey-3">bazattak007</span>
+                        </div>
+                        <div class="ft-left-p-rel w-100pc">
+                            <span class="txt-size-13px txt-weight-500 txt-white">72 assistindo</span>
+                        </div>
                     </div>
                 </div>
                 <div class="col-grid-videos col-thumb ft-left-p-rel">
                     <div class="container_thumbnail">
-                        <img src="//i.ytimg.com/vi_webp/WoGsRZVXYlg/mqdefault.webp" class="img-thumb" />
+                        <img src="https://i.ytimg.com/vi_webp/yq7LP17v8oY/hqdefault.webp" class="img-thumb" />
+                    </div>
+                    <div class="container-info-thumb-video">
+                        <div class="container-video-title-thumb">
+                            <span class="txt-size-15px txt-weight-500 txt-white">EA FIFA WORLD CUP 2018 A NOVELA CONTINUA</span>
+                        </div>
+                        <div class="ft-left-p-rel w-100pc">
+                            <span class="txt-size-13px txt-weight-400 txt-grey-3">bazattak007</span>
+                        </div>
+                        <div class="ft-left-p-rel w-100pc">
+                            <span class="txt-size-13px txt-weight-500 txt-white">72 assistindo</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <h1 class="h-h1">Recomendados</h1>
+            <div class="center-content-body al-center">
+                <div class="col-grid-videos col-thumb ft-left-p-rel">
+                    <div class="container_thumbnail">
+                        <img src="https://i.ytimg.com/vi/BpI_bUr1xvE/hqdefault_live.jpg" class="img-thumb" />
+                    </div>
+                    <div class="container-info-thumb-video">
+                        <div class="container-video-title-thumb">
+                            <span class="txt-size-15px txt-weight-500 txt-white">TOP 1 DEL MUNDO SEMANAL Y TOP 1 DE ESPAÑA | CALL OF DUTY: WW2</span>
+                        </div>
+                        <div class="ft-left-p-rel w-100pc">
+                            <span class="txt-size-13px txt-weight-400 txt-grey-3">bazattak007</span>
+                        </div>
+                        <div class="ft-left-p-rel w-100pc">
+                            <span class="txt-size-13px txt-weight-500 txt-white">72 assistindo</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-grid-videos col-thumb ft-left-p-rel">
+                    <div class="container_thumbnail">
+                        <img src="https://i.ytimg.com/vi/ZeY_PqYit_k/hqdefault_live.jpg" class="img-thumb" />
+                    </div>
+                    <div class="container-info-thumb-video">
+                        <div class="container-video-title-thumb">
+                            <span class="txt-size-15px txt-weight-500 txt-white">C9 vs. FLY - CLG vs. TSM - FOX vs. 100 - TL vs. OPT | NA LCS Spring (2018)</span>
+                        </div>
+                        <div class="ft-left-p-rel w-100pc">
+                            <span class="txt-size-13px txt-weight-400 txt-grey-3">bazattak007</span>
+                        </div>
+                        <div class="ft-left-p-rel w-100pc">
+                            <span class="txt-size-13px txt-weight-500 txt-white">72 assistindo</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-grid-videos col-thumb ft-left-p-rel">
+                    <div class="container_thumbnail">
+                        <img src="https://i.ytimg.com/vi/zcVXIj3M9ZQ/hqdefault_live.jpg" class="img-thumb" />
+                    </div>
+                    <div class="container-info-thumb-video">
+                        <div class="container-video-title-thumb">
+                            <span class="txt-size-15px txt-weight-500 txt-white">TOP 1 DEL MUNDO SEMANAL Y TOP 1 DE ESPAÑA | CALL OF DUTY: WW2</span>
+                        </div>
+                        <div class="ft-left-p-rel w-100pc">
+                            <span class="txt-size-13px txt-weight-400 txt-grey-3">bazattak007</span>
+                        </div>
+                        <div class="ft-left-p-rel w-100pc">
+                            <span class="txt-size-13px txt-weight-500 txt-white">72 assistindo</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-grid-videos col-thumb ft-left-p-rel">
+                    <div class="container_thumbnail">
+                        <img src="https://i.ytimg.com/vi/wDEwYYW8Pjk/hqdefault_live.jpg" class="img-thumb" />
+                    </div>
+                    <div class="container-info-thumb-video">
+                        <div class="container-video-title-thumb">
+                            <span class="txt-size-15px txt-weight-500 txt-white">TLA vs. FOXA | Semifinals Day 1 | NA Academy Spring Split (2018)</span>
+                        </div>
+                        <div class="ft-left-p-rel w-100pc">
+                            <span class="txt-size-13px txt-weight-400 txt-grey-3">bazattak007</span>
+                        </div>
+                        <div class="ft-left-p-rel w-100pc">
+                            <span class="txt-size-13px txt-weight-500 txt-white">72 assistindo</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-grid-videos col-thumb ft-left-p-rel">
+                    <div class="container_thumbnail">
+                        <img src="https://i.ytimg.com/vi/-TgREHxSp88/hqdefault_live.jpg" class="img-thumb" />
+                    </div>
+                    <div class="container-info-thumb-video">
+                        <div class="container-video-title-thumb">
+                            <span class="txt-size-15px txt-weight-500 txt-white">*NEW* HEAVY SHOTGUN - Fortnite Battle Royale LIVE</span>
+                        </div>
+                        <div class="ft-left-p-rel w-100pc">
+                            <span class="txt-size-13px txt-weight-400 txt-grey-3">bazattak007</span>
+                        </div>
+                        <div class="ft-left-p-rel w-100pc">
+                            <span class="txt-size-13px txt-weight-500 txt-white">72 assistindo</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-grid-videos col-thumb ft-left-p-rel">
+                    <div class="container_thumbnail">
+                        <img src="https://i.ytimg.com/vi/HgRtO4Q6JPI/hqdefault_live.jpg" class="img-thumb" />
+                    </div>
+                    <div class="container-info-thumb-video">
+                        <div class="container-video-title-thumb">
+                            <span class="txt-size-15px txt-weight-500 txt-white">RESIDENT EVIL 4 - SEM TOMAR DANO, LEVOU 1 HIT RESETA - PROFISSIONAL</span>
+                        </div>
+                        <div class="ft-left-p-rel w-100pc">
+                            <span class="txt-size-13px txt-weight-400 txt-grey-3">bazattak007</span>
+                        </div>
+                        <div class="ft-left-p-rel w-100pc">
+                            <span class="txt-size-13px txt-weight-500 txt-white">72 assistindo</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-grid-videos col-thumb ft-left-p-rel">
+                    <div class="container_thumbnail">
+                        <img src="https://i.ytimg.com/vi_webp/YTJxPiokCvk/hqdefault.webp" class="img-thumb" />
+                    </div>
+                    <div class="container-info-thumb-video">
+                        <div class="container-video-title-thumb">
+                            <span class="txt-size-15px txt-weight-500 txt-white">Live - Fifa 2018 Teste - Primeira vez que jogo FIFA - Direto do X360</span>
+                        </div>
+                        <div class="ft-left-p-rel w-100pc">
+                            <span class="txt-size-13px txt-weight-400 txt-grey-3">bazattak007</span>
+                        </div>
+                        <div class="ft-left-p-rel w-100pc">
+                            <span class="txt-size-13px txt-weight-500 txt-white">72 assistindo</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-grid-videos col-thumb ft-left-p-rel">
+                    <div class="container_thumbnail">
+                        <img src="https://i.ytimg.com/vi_webp/yq7LP17v8oY/hqdefault.webp" class="img-thumb" />
+                    </div>
+                    <div class="container-info-thumb-video">
+                        <div class="container-video-title-thumb">
+                            <span class="txt-size-15px txt-weight-500 txt-white">EA FIFA WORLD CUP 2018 A NOVELA CONTINUA</span>
+                        </div>
+                        <div class="ft-left-p-rel w-100pc">
+                            <span class="txt-size-13px txt-weight-400 txt-grey-3">bazattak007</span>
+                        </div>
+                        <div class="ft-left-p-rel w-100pc">
+                            <span class="txt-size-13px txt-weight-500 txt-white">72 assistindo</span>
+                        </div>
                     </div>
                 </div>
             </div>
