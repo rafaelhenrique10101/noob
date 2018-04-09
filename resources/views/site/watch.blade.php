78e1635e4eaf11a30c4b54d1dc2bbe5a620b3d36
Rafael Henrique <rafael.henrique@idealizza.com.br>
2018-04-09 15:29:22 -0300

    Ajustes nos botões small actions

--- resources/views/site/watch.blade.php
+++ resources/views/site/watch.blade.php
@@ -17,30 +17,36 @@
                     <div class="ft-left-p-rel pd-lt-10px wd-description-video">
                         <span class="w-100pc txt-size-15px txt-weight-500 txt-white font-rb">Official Call of Duty®: WWII Insider - Loadouts</span>
                         <span class="ft-right-p-rel txt-size-15px txt-weight-400 txt-grey-1 font-rb">1,234 visualizações</span>
-                        <span class="w-100pc ft-left-p-rel txt-size-13px txt-weight-400 txt-grey-1 font-rb">Publicado em 3 de jan de 2018</span>                        
+                        <span class="w-100pc ft-left-p-rel txt-size-13px txt-weight-400 txt-grey-1 font-rb"><span class="txt-size-13px txt-weight-500 txt-blue1 mg-rg-10px font-rb">Publisher</span>Publicado em 3 de jan de 2018</span>                        
                     </div>
                     <div class="ft-left-p-rel pd-lt-10px pd-tp-10px">
                         <span class="w-100pc ft-left-p-rel txt-size-14px txt-weight-400 txt-grey-1 font-rb">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI</span>
                     </div>
                     <div class="w-100pc ft-left-p-rel pd-lt-10px pd-tp-bt-10px row-border-separator">
                         <div class="ft-left-p-rel top-15px">                              
-                            <div class="ft-left-p-rel mg-lf-rg-15px">
-                                <img src="/img/svg/icons/ic_thumb_up_white_24px.svg">
+                            <div class="ft-left-p-rel mg-lf-rg-10px" style="line-height: 26px;">
+                                <button class="mdl-button mdl-js-button mdl-button--icon">
+                                    <img src="/img/svg/icons/ic_thumb_up_white_24px.svg">
+                                </button>
                                 <span class="ft-right-p-rel txt-size-14px txt-weight-400 pd-lt-10px pd-tp-5px txt-grey-1 font-rb">2,1 mil</span>
                             </div>
-                            <div class="ft-left-p-rel mg-lf-rg-15px">
-                                <img class="pd-tp-5px" src="/img/svg/icons/ic_thumb_down_white_24px.svg">
+                            <div class="ft-left-p-rel mg-lf-rg-10px" style="line-height: 26px;">
+                                <button class="mdl-button mdl-js-button mdl-button--icon">
+                                    <img class="pd-tp-5px" src="/img/svg/icons/ic_thumb_down_white_24px.svg">
+                                </button>
                                 <span class="ft-right-p-rel txt-size-14px txt-weight-400 pd-lt-10px pd-tp-5px txt-grey-1 font-rb">22</span>
                             </div>
-                            <div class="ft-left-p-rel mg-lf-rg-15px">
-                                <img src="/img/svg/icons/ic_share_white_24px.svg" style="padding-top: 2px;">                                
+                            <div class="ft-left-p-rel mg-lf-rg-10px">
+                                <button class="mdl-button mdl-js-button mdl-button--icon">
+                                    <img src="/img/svg/icons/ic_share_white_24px.svg" style="padding-top: 2px;">                                
+                                </button>
                             </div>                               
                         </div>
                         <div class="ft-right-p-rel">                              
                             <button type="button" class="btn-primary-grey mg-lt-10px mg-tp-bt-5px ft-right-p-rel font-rb">xxxxxxxx                                
-                            </button>
+                            </button>                            
                             <button type="button" class="btn-primary-green mg-lt-10px mg-tp-bt-5px ft-right-p-rel font-rb">matricule-se  
-                                <i class="fa fa-cart-plus mg-lt-5px"></i>                            
+                                <img src="/img/svg/icons/ic_add_shopping_cart_white_24px.svg" class="ft-left-p-rel mg-rg-5px" style="width: 20px;top: 1px;"/>
                             </button>                            
                         </div>
                     </div>
